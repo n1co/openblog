3c47fa6707687ec7bb9b4b72ad3cf89f7208c6b5
John Konolol <nico83110@gmail.com>
2013-08-21 23:09:17 +0200

    utf8 fixes for ajax requests

--- ajax_article.php
+++ ajax_article.php
@@ -9,8 +9,8 @@ while($data = mysql_fetch_array($result))
 {
 	?>
     <div class="article" id="<?php echo $data['id']?>">
-        <div class="article_header"><a href="perm.php?id=<?php echo $data['id']; ?>"><?php echo $data['title']; ?></a></div>
-        <div class="article_content"><?php echo $data['content']; ?></div>
+        <div class="article_header"><a href="perm.php?id=<?php echo $data['id']; ?>"><?php echo utf8_encode($data['title']); ?></a></div>
+        <div class="article_content"><?php echo utf8_encode($data['content']); ?></div>
         <div class="article_footer">Le <?php echo $data['created']; ?> par  <a href="#"><?php echo $data['author']; ?></a> in <?php echo $data['cat']; ?></div>
     </div>
 <?php
