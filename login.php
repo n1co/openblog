aaf7bfcb038d34d941fe10d39ca2a81097cddfc4
John Konolol <nico83110@gmail.com>
2013-08-21 22:17:55 +0200

    cleanup

--- login.php
+++ login.php
@@ -2,26 +2,28 @@
 include('includes/config.inc.php');
 if (isset($_POST['mail']))
 {
-$myusername=$_POST['mail']; 
-$mypassword=$_POST['password']; 
+    $myusername=$_POST['mail']; 
+    $mypassword=$_POST['password']; 
 
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
-$mypassword = pwd_encode($mypassword);
-$sql="SELECT * FROM users WHERE mail='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
+    $myusername = stripslashes($myusername);
+    $mypassword = stripslashes($mypassword);
+    $myusername = mysql_real_escape_string($myusername);
+    $mypassword = mysql_real_escape_string($mypassword);
+    $mypassword = pwd_encode($mypassword);
+    $sql="SELECT * FROM users WHERE mail='$myusername' and password='$mypassword'";
+    $result=mysql_query($sql);
 
-$count=mysql_num_rows($result);
+    $count=mysql_num_rows($result);
 
-if($count==1){
-$_SESSION['login']=$myusername;
-redirect_to('admin.php');
-}
-else {
-$smarty->assign('error', 'Invalid username and/or password.');
-}
+    if($count==1)
+    {
+        $_SESSION['login']=$myusername;
+        redirect_to('admin.php');
+    }
+    else 
+    {
+        $smarty->assign('error', 'Invalid username and/or password.');
+    }
 }
 $smarty->display("$theme/login.tpl");
 ?>
