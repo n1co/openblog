fd8dfc4f4d918d6eeb90eb2b1d8b1eedf574929b
John Konolol <nico83110@gmail.com>
2013-08-22 11:47:18 +0200

    stuff

--- templates/base/editor.tpl
+++ templates/base/editor.tpl
@@ -1,11 +1,15 @@
 {include file='base/header.tpl'}
+{literal}
 <script type="text/javascript">
-//<![CDATA[
-        bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
-  //]]>
+bkLib.onDomLoaded(function(){
+  new nicEditor({fullPanel : true, onSave : function(content, id, instance) {
+    alert('Work in progress. Use the bottom button instead.');
+  } }).panelInstance('editor');
+});
+{/literal}
   </script>
 <form method="POST" action="editor.php?id={$postId}">
-<input name="title" id="editor" type="text" class="input-block-level" value="{$postTitle}">
+<input name="title" type="text" class="input-block-level" value="{$postTitle}">
 <textarea id="editor" name="content" style="width: 100%; height: 300px;">
        {$postContent}
 </textarea>
--- templates_c/d486497e1ab2e69901915afd6f1c4f84e33150b1.file.editor.tpl.php
+++ templates_c/d486497e1ab2e69901915afd6f1c4f84e33150b1.file.editor.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2013-08-20 16:27:56
+<?php /* Smarty version Smarty-3.1.14, created on 2013-08-22 11:44:31
          compiled from "templates/base/editor.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:211466721252137988aebe42-36473599%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'd486497e1ab2e69901915afd6f1c4f84e33150b1' => 
     array (
       0 => 'templates/base/editor.tpl',
-      1 => 1377008873,
+      1 => 1377164669,
       2 => 'file',
     ),
   ),
@@ -27,14 +27,18 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_52137988b1dcb3_65433440')) {function content_52137988b1dcb3_65433440($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('base/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
+
 <script type="text/javascript">
-//<![CDATA[
-        bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
-  //]]>
+bkLib.onDomLoaded(function(){
+  new nicEditor({fullPanel : true, onSave : function(content, id, instance) {
+    $.post("editor.php", { title: "title", content: "content" } );
+  } }).panelInstance('editor');
+});
+
   </script>
 <form method="POST" action="editor.php?id=<?php echo $_smarty_tpl->tpl_vars['postId']->value;?>
 ">
-<input name="title" id="editor" type="text" class="input-block-level" value="<?php echo $_smarty_tpl->tpl_vars['postTitle']->value;?>
+<input name="title" type="text" class="input-block-level" value="<?php echo $_smarty_tpl->tpl_vars['postTitle']->value;?>
 ">
 <textarea id="editor" name="content" style="width: 100%; height: 300px;">
        <?php echo $_smarty_tpl->tpl_vars['postContent']->value;?>
